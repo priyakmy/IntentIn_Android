42844c58188d47a084b156c2187428c4c3ac0869
priya Kumari <priyakum1071@gmail.com>
2024-04-10 11:26:54 +0530

    Explicit Intent

--- app/src/main/java/com/example/intent_filter/MainActivity.kt
+++ app/src/main/java/com/example/intent_filter/MainActivity.kt
@@ -2,19 +2,25 @@ package com.example.intent_filter
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.intent_filter.SecondActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val clickbutton = findViewById<Button>(R.id.btnNext)
 
-        // Create an Intent to start SecondActivity
-        val intent = Intent(this, SecondActivity::class.java)
+            clickbutton.setOnClickListener{
+            // Create an Intent to start SecondActivity
+            val intent = Intent(this , SecondActivity::class.java)
 
-        // Start SecondActivity using the intent
-        startActivity(intent)
+            // Start SecondActivity using the intent
+            startActivity(intent)
+                finish()
+        }
     }
 }
--- app/src/main/java/com/example/intent_filter/ThirdActivity.kt
+++ app/src/main/java/com/example/intent_filter/ThirdActivity.kt
@@ -1,8 +1,13 @@
 package com.example.intent_filter
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Telephony.Mms.Intents
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -17,6 +22,22 @@ class ThirdActivity : AppCompatActivity()
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left , systemBars.top , systemBars.right , systemBars.bottom)
             insets
+        }
+        val webButton = findViewById<CardView>(R.id.cardWeb)
+        val cameraButton = findViewById<CardView>(R.id.cardCamera)
+
+        webButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW)
+            // Pass Uri/URL of any website
+            intent.data= Uri.parse("https://www.youtube.com/")
+            startActivity(intent)
+
+        }
+
+        cameraButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+
         }
     }
 }
